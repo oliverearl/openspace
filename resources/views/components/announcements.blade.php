1f37ab41265fd753773d0dced8ca63f0607e0024
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-11 23:06:06 +0100

    feat: WIP frontend components for frontpage x2

---
+++ resources/views/components/announcements.blade.php
@@ -0,0 +1,35 @@
+<div class="specials">
+    <div class="heading">
+        <h4>{{ Config::string('app.name') }} Announcements</h4>
+    </div>
+
+    {{-- TODO: Yet another component that needs actually building. --}}
+    <div class="inner">
+        <div class="image">
+            <a href="https://store.remilia.org/">
+                <img
+                    src="https://store.remilia.org/cdn/shop/files/2_1_180x.gif"
+                    alt="Merchandise Photo"
+                    loading="lazy"
+                />
+            </a>
+        </div>
+
+        <div class="details">
+            <h4>
+                <h4>
+                    <a href="https://store.remilia.org/">REMILIA Merchandise</a>
+                </h4>
+                <p>
+                    <em>Now available!</em> Support REMILIA by buying a high-quality Shirt, Hoodie, or hat!
+                    <span class="m-hide">Check out the full Collection on
+                        <a href="https://store.remilia.org">store.remilia.org</a>!
+                    </span>
+                </p>
+                <p>
+                    <strong>&raquo; <a href="https://store.remilia.org/">Shop Now!</a></strong>
+                </p>
+            </h4>
+        </div>
+    </div>
+</div>
