87542c0a2fa75d76647e4917924fde81bba7760a
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-13 13:02:37 +0100

    test: set up initial tests for fortify behaviour

---
+++ tests/Feature/Authentication/LoginTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\User;
+
+it('can log into an account', function (): void {
+    $user = User::factory()->create();
+
+    $this->post(route('login'), [
+        'email' => $user->email,
+        'password' => 'password',
+        'password_confirmation' => 'password',
+        'remember' => true,
+    ])
+        ->assertRedirectToRoute('home');
+
+    $this->assertAuthenticatedAs($user);
+});
+
+it('will redirect in response to a login attempt when already authenticated', function (): void {
+    $authenticatedUser = User::factory()->create();
+    $this->actingAs($authenticatedUser);
+
+    $anotherUser = User::factory()->create();
+
+    $this->post(route('login'), [
+        'email' => $anotherUser->email,
+        'password' => 'password',
+        'password_confirmation' => 'password',
+        'remember' => true,
+    ])
+        ->assertRedirectToRoute('home');
+
+    $this->assertAuthenticatedAs($authenticatedUser);
+});
+
+it('will not authenticate incorrect credentials', function (): void {
+    $user = User::factory()->create();
+    $wrongPassword = 'not_password';
+
+    expect(password_verify($wrongPassword, $user->password))->toBeFalse();
+
+    $this->post(route('login'), [
+        'email' => $user->email,
+        'password' => $wrongPassword,
+        'password_confirmation' => $wrongPassword,
+        'remember' => true,
+    ])
+        ->assertRedirectToRoute('homepage');
+
+    $this->assertGuest();
+});
