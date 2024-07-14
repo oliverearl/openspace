bcc9f71b1cad8c67b3bf3f521d3c15abbb0da603
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-14 01:52:51 +0100

    fix: would help to remove the old markup

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -67,35 +67,4 @@
     </div>
 
     <x-info-area />
-
-    <div class="row info-area">
-        <div class="col info-box">
-            <h3>Retro Social</h3>
-            <p>All the things you missed most about Social Networks are back: Bulletins, Blogs, Forums, and so
-                much more!</p>
-            <p class="link">&raquo; <a href="register.php"
-                                       title="Join {{ Config::string('app.name') }} Today">Join Today</a></p>
-        </div>
-        <div class="col info-box">
-            <h3>Privacy Friendly</h3>
-            <p>No algorithms, no tracking, no personalized Ads - just a safe space for you and your friends to
-                hang out online!</p>
-            <p class="link">&raquo; <a href="browse.php"
-                                       title="Browse {{ Config::string('app.name') }} Profiles">Browse Profiles</a></p>
-        </div>
-        <div class="col info-box">
-            <h3>Fully Customizable</h3>
-            <p>Featuring custom HTML and CSS to give you all the freedom you need to make your Profile truly
-                <i>your</i> Space on the web!
-            </p>
-            <p class="link">&raquo; <a href="layouts/"
-                                       title="Discover custom {{ Config::string('app.name') }} Layouts">Discover Layouts</a></p>
-        </div>
-        <div class="col info-box">
-            <h3>Join Today!</h3>
-            <p>Join your friends on the web or meet some new ones.</p>
-            <p class="link">&raquo; <a href="register.php"
-                                       title="Sign Up for {{ Config::string('app.name') }}">SignUp Now</a></p>
-        </div>
-    </div>
 @endsection
