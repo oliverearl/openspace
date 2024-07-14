0efe6f93cd2dd6856611fd0009149ff1bc138c69
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-14 16:53:14 +0100

    refactor: adjust indie box into its own component

---
+++ resources/views/components/indie-box.blade.php
@@ -0,0 +1,10 @@
+<div class="indie-box">
+    <p>
+        {{ Config::string('app.name') }} is an open source social network. Check out the code and host your
+        own instance!
+    </p>
+
+    <p>
+        <a href="{{ Config::string('openspace.social.github') }}" class="more-details">[more details]</a>
+    </p>
+</div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -53,16 +53,7 @@
                 Stuff goes here.
             </div>
 
-            <div class="indie-box">
-                <p>
-                    {{ Config::string('app.name') }} is an open source social network. Check out the code and host your
-                    own instance!
-                </p>
-
-                <p>
-                    <a href="{{ Config::string('openspace.social.github') }}" class="more-details">[more details]</a>
-                </p>
-            </div>
+            <x-indie-box />
         </div>
     </div>
 
