5df8bfc70161f5313b416011a714f8a561d70f31
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-15 23:22:50 +0100

    test: user tests

---
+++ tests/Integration/Models/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Carbon;
+
+beforeEach(function (): void {
+   $this->user = User::factory()->create();
+});
+
+it('can cast its date fields into carbon objects', function (): void {
+   expect($this->user->email_verified_at)
+       ->toBeInstanceOf(Carbon::class)
+       ->and($this->user->last_active_at)->toBeInstanceOf(CarbonImmutable::class)
+       ->and($this->user->last_logged_in_at)->toBeInstanceOf(CarbonImmutable::class)
+       ->and($this->user->created_at)->toBeInstanceOf(CarbonImmutable::class)
+       ->and($this->user->updated_at)->toBeInstanceOf(CarbonImmutable::class);
+});
+
+it('hashes its passwords', function (): void {
+   expect($this->user->password)
+       ->not()->toEqual('password')
+       ->and(password_verify('password', $this->user->password))->toBeTrue();
+});
