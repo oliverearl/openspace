7fbc914df3428774e8f7980080ae547d96e0a422
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-15 23:10:12 +0100

    feat: add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(InfoBoxSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            InfoBoxSeeder::class,
+        ]);
     }
 }
