6adcf88f09cedf78c6038d686c6d3504c685e00a
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-18 22:24:42 +0100

    feat: repository to show latest users that fit the bill

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Support\Collection;
+
+class UserRepository
+{
+    /**
+     * Return a collection of the latest, verified users that have display pictures.
+     *
+     * @return Collection<int, \App\Models\User>
+     */
+    public function getLatestUsers(int $limit = 6): Collection
+    {
+        return User::query()
+            ->withWhereHas('media')
+            ->whereNotNull('email_verified_at')
+            ->whereNotNull('last_logged_in_at')
+            ->limit($limit)
+            ->latest()
+            ->get();
+    }
+}
