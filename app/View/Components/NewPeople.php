d0ef2879d7edfa6b5a73568b1929b78fc49b1641
Oliver Earl <oliver@oliverearl.co.uk>
2024-07-18 22:37:32 +0100

    feat: new people component fully fleshed out

--- app/View/Components/NewPeople.php
+++ app/View/Components/NewPeople.php
@@ -2,18 +2,31 @@
 
 namespace App\View\Components;
 
+use App\Repositories\UserRepository;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\Component;
 
 class NewPeople extends Component
 {
-    public readonly Collection $people;
+    /**
+     * The key that can be used to bust the new people cache.
+     */
+    public const string NEW_PEOPLE_KEY = 'new_people_key';
 
-    public function __construct()
+    /**
+     * Busts the new people cache and forces it to reload.
+     */
+    public static function reset(): void
     {
-        $this->people = collect(); // TODO: Populate with a database query.
+        Cache::forget(self::NEW_PEOPLE_KEY);
     }
+
+    /**
+     * Constructs a new instance of the new people component.
+     */
+    public function __construct(public readonly UserRepository $repository) {}
 
     /**
      * Get the view / contents that represent the component.
@@ -21,7 +34,20 @@ class NewPeople extends Component
     public function render(): View
     {
         return view('components.new-people', [
-            'people' => $this->people,
+            'people' => $this->getLatestUsers(),
         ]);
+    }
+
+    /**
+     * Retrieve the latest users.
+     *
+     * @return \Illuminate\Support\Collection<int, \App\Models\User>
+     */
+    private function getLatestUsers(): Collection
+    {
+        return Cache::rememberForever(
+            self::NEW_PEOPLE_KEY,
+            fn(): Collection => $this->repository->getLatestUsers(),
+        );
     }
 }
