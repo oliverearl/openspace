0b47f01f72a17e664c38661e7aef7c5b32d2b29f
Oliver Earl <oliver@oliverearl.co.uk>
2025-01-25 15:39:24 +0000

    style: pint+strict

--- database/migrations/2024_07_12_231755_add_two_factor_columns_to_users_table.php
+++ database/migrations/2024_07_12_231755_add_two_factor_columns_to_users_table.php
@@ -1,14 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Laravel\Fortify\Fortify;
 
-return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
@@ -28,9 +27,6 @@ return new class () extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
--- database/migrations/2024_07_15_222657_create_media_table.php
+++ database/migrations/2024_07_15_222657_create_media_table.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('media', function (Blueprint $table) {
@@ -27,5 +29,10 @@ return new class () extends Migration {
 
             $table->nullableTimestamps();
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
     }
 };
